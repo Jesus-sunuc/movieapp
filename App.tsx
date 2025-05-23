8341945fa5a7f6badefa305c17f504bc634742fe
Jesus Sunuc <mariano.code4guate@gmail.com>
2025-05-23 12:38:26 -0600

    cleaned up

--- App.tsx
+++ App.tsx
@@ -1,16 +1,9 @@
 import React from 'react';
 import { useColorScheme} from 'react-native';
 import {GestureHandlerRootView} from 'react-native-gesture-handler';
-import {Colors} from 'react-native/Libraries/NewAppScreen';
 import Home from './src/screens/Home/Home';
 
 function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-    flex: 1,
-  };
 
   return (
     <GestureHandlerRootView style={{flex: 1}}>
