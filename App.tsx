d597fa87003e8d1dbdf9800a0163efeb7ec51efa
Jesus Sunuc <mariano.code4guate@gmail.com>
2025-05-24 14:37:10 -0600

    Stack Navigator implemented

--- App.tsx
+++ App.tsx
@@ -1,11 +1,20 @@
 import React from 'react';
 import {GestureHandlerRootView} from 'react-native-gesture-handler';
 import Home from './src/screens/Home/Home';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
 
 function App(): React.JSX.Element {
 
   return (
     <GestureHandlerRootView style={{flex: 1}}>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Home" component={Home} options={{headerShown: false}}/>
+        </Stack.Navigator>
+      </NavigationContainer>
       <Home />
     </GestureHandlerRootView>
   );
