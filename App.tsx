11da99484c79e8c972f37edc1d23a0fc111d928d
Jesus Sunuc <mariano.code4guate@gmail.com>
2025-05-24 14:59:51 -0600

    Tab screen

--- App.tsx
+++ App.tsx
@@ -1,21 +1,40 @@
 import React from 'react';
 import {GestureHandlerRootView} from 'react-native-gesture-handler';
 import Home from './src/screens/Home';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import {NavigationContainer} from '@react-navigation/native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import Search from './src/screens/Search';
+import WishList from './src/screens/WishList';
+import Profile from './src/screens/Profile';
 
-const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
 
 function App(): React.JSX.Element {
-
   return (
     <GestureHandlerRootView style={{flex: 1}}>
       <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen name="Home" component={Home} options={{headerShown: false}}/>
-        </Stack.Navigator>
+        <Tab.Navigator
+          screenOptions={{
+            headerShown: false,
+          }}>
+          <Tab.Screen 
+            name="Home" 
+            component={Home}
+          />
+          <Tab.Screen 
+            name="Search" 
+            component={Search}
+          />
+          <Tab.Screen 
+            name="WishList" 
+            component={WishList}
+          />
+          <Tab.Screen 
+            name="Profile" 
+            component={Profile}
+          />
+        </Tab.Navigator>
       </NavigationContainer>
-      <Home />
     </GestureHandlerRootView>
   );
 }
